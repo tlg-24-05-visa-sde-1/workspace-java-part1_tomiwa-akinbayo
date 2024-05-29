437267259914ceb693aa2dd9fc2643637630fab6
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-05-29 16:58:40 -0500

    Add fileReturn() as default method in TaxPayer

--- Lab09.1/src/com/hr/personnel/Executive.java
+++ Lab09.1/src/com/hr/personnel/Executive.java
@@ -15,6 +15,11 @@ public class Executive extends SalariedEmployee {
         System.out.println(getName() + " enjoying a nice round of golf");
     }
 
+    @Override
+    public void fileReturn() {
+        System.out.println("Return filled in person in front of congress");
+    }
+
     // would only need separate payTaxes() if taxes were different from Salaried employees
 //    public void payTaxes() {
 //        System.out.println(getName() + " paid taxes of " + (getSalary() * SALARIED_TAX_RATE));
